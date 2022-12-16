ef50f121291f34f31cb23ac68768a600ff1aff08
Baybosunov Chyngyz <baybosunov0411@gmail.com>
2022-12-16 20:03:25 +0700

    file

---
+++ tg_bot_template/bot.py
@@ -0,0 +1,6 @@
+# Основной файл проекта - точка входа в проект. Запуск бота - это, по сути, запуск файла bot.py
+
+from config_data.config import load_config
+
+
+config = load_config('<путь к файлу .env>')
